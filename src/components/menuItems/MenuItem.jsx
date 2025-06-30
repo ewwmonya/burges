b3cceb19747efba58a6da0e74f2b7567f4cfb454
ewwmonya <savvymallen85@gmail.com>
2025-06-29 20:14:24 -0400

    Fixed Town favs in mobile

--- src/components/menuItems/MenuItem.jsx
+++ src/components/menuItems/MenuItem.jsx
@@ -2,16 +2,13 @@ function MenuItem({ item }) {
 	const { category, name, price, description, image } = item;
 	return (
 		<>
-			<div className='card bg-base-200 w-60 shadow-md'>
+			<div className='card bg-base-200 lg:w-60 w-55 shadow-md'>
 				<figure className='overflow-hidden aspect-square max-h-[200px]'>
 					<img src={image} alt='Shoes' />
 				</figure>
 				<div className='card-body'>
 					<h2 className='card-title'>{name}</h2>
 					<p>{description}</p>
-					<div className='card-actions flex justify-end'>
-						<button className='btn btn-neutral'>Buy Now</button>
-					</div>
 				</div>
 			</div>
 		</>
